68f2060ab4895a5b8e88c71e370d4aece00da80d
Gunjan Kumar <sngunjan@yahoo.com>
2017-02-17 17:06:29 -0500

    added in memory db in place of files

---
+++ src/main/java/gunjan/dao/JDBCInMemory.java
@@ -0,0 +1,125 @@
+package gunjan.dao;
+
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.sql.*;
+
+/**
+ * Created by GK82893 on 2/17/2017.
+ */
+@Component
+public class JDBCInMemory {
+    // JDBC driver name and database URL
+    static final String JDBC_DRIVER = "org.h2.Driver"; //org.h2.Driver
+    static final String DB_URL = "jdbc:h2:~/test";
+
+    //  Database credentials
+    static final String USER = "sa";
+    static final String PASS = "";
+@PostConstruct
+    public void createTable() {
+        Connection conn = null;
+        Statement stmt = null;
+
+        try {
+            //STEP 2: Register JDBC driver
+            Class.forName(JDBC_DRIVER );
+
+            //STEP 3: Open a connection
+            System.out.println("Connecting to a selected database...");
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            System.out.println("Connected database successfully...");
+
+            //STEP 4: Execute a query
+            System.out.println("Creating table in given database...");
+            stmt = conn.createStatement();
+
+            String sql = "CREATE TABLE IF NOT EXISTS USER_APP_KEY " +
+                    "(app_id VARCHAR(255) not NULL, " +
+                    " public_key VARCHAR(MAX), " +
+                    " private_key VARCHAR(MAX)) ";
+
+            stmt.executeUpdate(sql);
+            System.out.println("Created table in given database...");
+        } catch (SQLException se) {
+            //Handle errors for JDBC
+            se.printStackTrace();
+        } catch (Exception e) {
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        } finally {
+            //finally block used to close resources
+            try {
+                if (stmt!=null)
+                    conn.close();
+            } catch (SQLException se) {
+            } // do nothing
+            try {
+                if (conn!= null)
+                    conn.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            } // end finally try
+        } // end try
+        System.out.println("Goodbye!");
+    //this.insertData();
+    } // end main
+
+    public void insertData (String appId, String publicKey, String privateKey) {
+        Connection conn = null;
+        Statement stmt = null;
+
+        try {
+            //STEP 2: Register JDBC driver
+            Class.forName(JDBC_DRIVER);
+
+            //STEP 3: Open a connection
+            System.out.println("Connecting to a selected database...");
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            System.out.println("Connected database successfully...");
+
+            //STEP 4: Execute a query
+            System.out.println("Inserting records into the table...");
+            stmt = conn.createStatement();
+            //if appid exist
+            String sql = "SELECT APP_ID FROM USER_APP_KEY WHERE APP_ID='"+appId+"'";
+            ResultSet resultSet = stmt.executeQuery(sql);
+            if(!resultSet.isBeforeFirst())
+            {
+                stmt.executeUpdate("INSERT INTO USER_APP_KEY " + "VALUES ('"+appId+"', '"+publicKey+"', '"+privateKey+"')");
+                resultSet.close();
+            }
+            else
+            {
+                stmt.executeUpdate("UPDATE USER_APP_KEY SET PUBLIC_KEY='"+publicKey+"', PRIVATE_KEY='"+privateKey+"' WHERE APP_ID='"+appId+"' ");
+                resultSet.close();
+            }
+            System.out.println("Inserted records into the table...");
+
+        } catch (SQLException se) {
+            //Handle errors for JDBC
+            se.printStackTrace();
+        } catch (Exception e) {
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        } finally {
+            //finally block used to close resources
+            try {
+                if (stmt!=null)
+                    conn.close();
+            } catch (SQLException se) {
+            } // do nothing
+            try {
+                if (conn!=null)
+                    conn.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            } //end finally try
+        } //end try
+
+        System.out.println("Goodbye!");
+
+    } //end main
+}
+
--- src/main/java/gunjan/utils/GenerateKeys.java
+++ src/main/java/gunjan/utils/GenerateKeys.java
@@ -3,19 +3,25 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
-import java.security.PrivateKey;
-import java.security.PublicKey;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.*;
+import java.security.spec.PKCS8EncodedKeySpec;
 
 import javax.xml.bind.DatatypeConverter;
 
+import com.sun.org.apache.xml.internal.security.exceptions.Base64DecodingException;
+import com.sun.org.apache.xml.internal.security.utils.Base64;
+import gunjan.dao.JDBCInMemory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-
+@Component
 public class GenerateKeys {
+
+	@Autowired
+	private JDBCInMemory jdbcInMemory;
 
 	private KeyPairGenerator keyGen;
 	private KeyPair pair;
@@ -27,9 +33,8 @@ public class GenerateKeys {
 	static final String PRIVATEKEY_POSTFIX  = "-----END RSA PRIVATE KEY-----";
 	
 	
-	
-
-	public GenerateKeys(int keylength) throws NoSuchAlgorithmException, NoSuchProviderException {
+	public GenerateKeys(){};
+	public void generateSecureKeys(int keylength) throws NoSuchAlgorithmException, NoSuchProviderException {
 		this.keyGen = KeyPairGenerator.getInstance("RSA");
 		this.keyGen.initialize(keylength);
 	}
@@ -47,53 +52,58 @@ public class GenerateKeys {
 	public PublicKey getPublicKey() {
 		return this.publicKey;
 	}
-
-	public void writeToFile(String path, String key) throws IOException {
-		File f = new File(path);
-		f.getParentFile().mkdirs();
-		FileWriter fw = new FileWriter(f);
-		fw.write(key);
-		fw.close();
-
-	}
-	public void writeToFile(String path, byte[] key) throws IOException {
-		File f = new File(path);
-		f.getParentFile().mkdirs();
-
-		FileOutputStream fos = new FileOutputStream(f);
-		fos.write(key);
-		fos.flush();
-		fos.close();
+//
+//	public void writeToFile(String path, String key) throws IOException {
+//		File f = new File(path);
+//		f.getParentFile().mkdirs();
+//		FileWriter fw = new FileWriter(f);
+//		fw.write(key);
+//		fw.close();
+//
+//
+//	}
+	private void saveKeysInDb (String appId, String publicKey, String privateKey)
+	{
+		this.jdbcInMemory.insertData(appId, publicKey, privateKey);
 	}
 
-	public static String keyGenerateAndReturnPublicKey() {
-		GenerateKeys gk;
+
+	public String keyGenerateAndReturnPublicKey(String appId) {
+		//GenerateKeys gk;
 		String publicKeyPEM = null;
 		String privateKeyPEM = null;
 		System.out.println("main method of generator");
 		try {
-			gk = new GenerateKeys(512);
-			gk.createKeys();
-
+			//gk = new GenerateKeys(1024);
+			//gk.createKeys();
+			this.generateSecureKeys(1024);
+			this.createKeys();
 
 			// THIS IS PEM:
-	        publicKeyPEM = PUBLICKEY_PREFIX + "\n" + DatatypeConverter.printBase64Binary(gk.getPublicKey().getEncoded()).replaceAll("(.{64})", "$1\n") + "\n" + PUBLICKEY_POSTFIX;
-	        privateKeyPEM = PRIVATEKEY_PREFIX + "\n" + DatatypeConverter.printBase64Binary(gk.getPrivateKey().getEncoded()).replaceAll("(.{64})", "$1\n") + "\n" + PRIVATEKEY_POSTFIX;
-
+	        publicKeyPEM = PUBLICKEY_PREFIX + "\n" + DatatypeConverter.printBase64Binary(this.getPublicKey().getEncoded()).replaceAll("(.{64})", "$1\n") + "\n" + PUBLICKEY_POSTFIX;
+	        privateKeyPEM = PRIVATEKEY_PREFIX + "\n" + DatatypeConverter.printBase64Binary(this.getPrivateKey().getEncoded()).replaceAll("(.{64})", "$1\n") + "\n" + PRIVATEKEY_POSTFIX;
 	        
-	        
-	        gk.writeToFile("KeyPair/publicKey", publicKeyPEM);
-			gk.writeToFile("KeyPair/privateKey", gk.getPrivateKey().getEncoded());
-
+	        //gk.writeToFile("KeyPair/publicKey", publicKeyPEM);
+			//gk.writeToFile("KeyPair/privateKey", privateKeyPEM);
+			this.saveKeysInDb(appId,publicKeyPEM,privateKeyPEM);
 			
-			
-		} catch (NoSuchAlgorithmException | NoSuchProviderException e) {
-			System.err.println(e.getMessage());
-		} catch (IOException e) {
+		} catch (Exception e) {
 			System.err.println(e.getMessage());
 		}
-		return publicKeyPEM+"::::::::"+privateKeyPEM;
-
+		return publicKeyPEM;
+	}
+	public static PrivateKey readPrivateKey()
+			throws IOException, GeneralSecurityException, Base64DecodingException {
+		PrivateKey key = null;
+		String fileString = new String(Files.readAllBytes(Paths.get("KeyPair/privateKey")), StandardCharsets.UTF_8);
+		fileString = fileString.replace(
+				"-----BEGIN RSA PRIVATE KEY-----\n", "")
+				.replace("-----END RSA PRIVATE KEY-----", "");
+		byte [] keyBytes = Base64.decode(fileString);
+		PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(keyBytes);
+		KeyFactory kf = KeyFactory.getInstance("RSA");
+		key = kf.generatePrivate(spec);
+		return key;
 	}
 
 }
