e30c546ed68511066e418e82411574e020da60d1
gunjank <sngunjan@yahoo.com>
2017-02-16 21:00:08 -0500

    added initial files

---
+++ src/main/java/gunjan/controller/GreetingController.java
@@ -0,0 +1,22 @@
+//package gunjan.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//
+//@Controller
+//public class GreetingController {
+//
+//    @RequestMapping("/greeting")
+//    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+//        model.addAttribute("name", name);
+//        return "greeting";
+//    }
+////    @RequestMapping("/key")
+////    public String key(@RequestParam(value="appId", required=true) String name) {
+////       // model.addAttribute("name", name);
+////        return "greeting";
+////    }
+//
+//}
